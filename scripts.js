997b009849463eac8062a4d04670c1b141cb2fdc
Julita Lutostańska <julita.lutostanska@gmail.com>
2017-12-03 15:53:29 +0100

    Change scripts

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 
 var allNames = femaleNames.concat(maleNames);
-console.log=(allNames);
+console.log(allNames);
 
 var newName = 'Marian';
 
@@ -10,4 +10,4 @@ if (allNames.indexOf(newName) === -1) {
 	var addNewName = allNames.push(newName);
 }
 
-console.log=(allNames);
+console.log(allNames);
